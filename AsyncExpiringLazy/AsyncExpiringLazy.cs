41ab87377056ad075d7058d393713410be407115
filipw <wojcieszyn@gmail.com>
2016-11-01 16:08:24 +0100

    v1

---
+++ AsyncExpiringLazy/AsyncExpiringLazy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncExpiringLazy
+{
+    public class AsyncExpiringLazy<T>
+    {
+        private static readonly SemaphoreSlim SyncLock = new SemaphoreSlim(initialCount: 1);
+        private readonly Func<ExpirationMetadata<T>, Task<ExpirationMetadata<T>>> _valueProvider;
+        private DateTimeOffset? _validUntil;
+        private ExpirationMetadata<T> _value;
+
+        public AsyncExpiringLazy(Func<ExpirationMetadata<T>, Task<ExpirationMetadata<T>>> valueProvider)
+        {
+            if (valueProvider == null) throw new ArgumentNullException(nameof(valueProvider));
+            _valueProvider = valueProvider;
+        }
+
+        public bool IsValueCreated => _validUntil != null && _validUntil > DateTimeOffset.UtcNow && _value.Result != null;
+
+        public async Task<T> Value()
+        {
+            if (!IsValueCreated)
+            {
+                await SyncLock.WaitAsync();
+                try
+                {
+                    var result = await _valueProvider(_value).ConfigureAwait(false);
+                    _value = result;
+                    _validUntil = result.ValidUntil;
+                }
+                finally
+                {
+                    SyncLock.Release();
+                }
+            }
+
+            return _value.Result;
+        }
+
+        public void Invalidate()
+        {
+            _validUntil = null;
+        }
+    }
+}
---
+++ AsyncExpiringLazy/ExpirationMetadata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AsyncExpiringLazy
+{
+    public struct ExpirationMetadata<T>
+    {
+        public T Result { get; set; }
+
+        public DateTimeOffset ValidUntil { get; set; }
+    }
+}
