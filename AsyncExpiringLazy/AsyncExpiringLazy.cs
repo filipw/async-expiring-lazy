3823ea59ca4e47272fe6cd4517be47fb20e955b9
Filip W <wojcieszyn@gmail.com>
2016-11-02 08:26:44 +0100

    Update AsyncExpiringLazy.cs

--- AsyncExpiringLazy/AsyncExpiringLazy.cs
+++ AsyncExpiringLazy/AsyncExpiringLazy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -18,30 +18,53 @@ namespace Strathweb
             _valueProvider = valueProvider;
         }
 
-        public bool IsValueCreated => _value.Result != null && _value.ValidUntil > DateTimeOffset.UtcNow;
+        private bool IsValueCreatedInternal => _value.Result != null && _value.ValidUntil > DateTimeOffset.UtcNow;
+
+        public async Task<bool> IsValueCreated()
+        {
+            await _syncLock.WaitAsync().ConfigureAwait(false);
+            try
+            {
+                return IsValueCreatedInternal;
+            }
+            finally
+            {
+                _syncLock.Release();
+            }
+        }
 
         public async Task<T> Value()
         {
-            if (!IsValueCreated)
+            await _syncLock.WaitAsync().ConfigureAwait(false);
+            try
             {
-                await _syncLock.WaitAsync().ConfigureAwait(false);
-                try
+                if (IsValueCreatedInternal)
                 {
-                    var result = await _valueProvider(_value).ConfigureAwait(false);
-                    _value = result;
-                }
-                finally
-                {
-                    _syncLock.Release();
+                    return _value.Result;
                 }
             }
+            finally
+            {
+                _syncLock.Release();
+            }
 
-            return _value.Result;
+            await _syncLock.WaitAsync();
+            try
+            {
+                var result = await _valueProvider(_value).ConfigureAwait(false);
+                _value = result;
+                return _value.Result;
+
+            }
+            finally
+            {
+                _syncLock.Release();
+            }
         }
 
-        public void Invalidate()
+        public async Task Invalidate()
         {
-            _syncLock.Wait();
+            await _syncLock.WaitAsync().ConfigureAwait(false);
             _value = default(ExpirationMetadata<T>);
             _syncLock.Release();
         }
